a1cdc02d99a8c6051b70eac8272395ea7f8c2207
jiegto <jiegto@outlook.com>
2020-11-03 17:51:43 +0800

    Update smartReplace.js

--- smartReplace.js
+++ smartReplace.js
@@ -3,7 +3,7 @@ async function replaceWithSecrets(content, Secrets, ext) {
     if (!Secrets || !Secrets) return content;
     const replacements = [];
     await init_notify(Secrets, content, replacements);
-    if (ext && typeof ext == "string") {
+    for (ext && typeof ext == "string") {
         (content.indexOf("require('./jdCookie.js')") > 0) {
             replacements.push({ key: "require('./jdCookie.js')", value: `{CookieJD:'${ext}'}` });
         }
