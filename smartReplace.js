e9c540f17283c05179367964fb827f8756a9759e
jiegto <jiegto@outlook.com>
2020-11-03 19:17:51 +0800

    Update smartReplace.js

--- smartReplace.js
+++ smartReplace.js
@@ -3,17 +3,7 @@ async function replaceWithSecrets(content, Secrets, ext) {
     if (!Secrets || !Secrets) return content;
     const replacements = [];
     await init_notify(Secrets, content, replacements);
-    if (ext && typeof ext == "string") {
-        if (content.indexOf("require('./jdCookie.js')") > 0) {
-            replacements.push({ key: "require('./jdCookie.js')", value: `{CookieJD:'${ext}'}` });
-        }
-    } else {
-        if (Secrets.JD_COOKIE && content.indexOf("require('./jdCookie.js')") > 0) {
-            replacements.push({ key: "require('./jdCookie.js')", value: JSON.stringify(Secrets.JD_COOKIE.split("&")) });
-        }
-        await downloader(content);
-    }
-    return batchReplace(content, replacements);
+    if (ext && typeof ext == "string")  return batchReplace(content, replacements);
 }
 function batchReplace(content, replacements) {
     for (var i = 0; i < replacements.length; i++) {
@@ -22,16 +12,7 @@ function batchReplace(content, replacements) {
     return content;
 }
 
-async function init_notify(Secrets, content, replacements) {
-        if (content.indexOf("require('./sendNotify')") > 0) {
-            replacements.push({
-                key: "require('./sendNotify')",
-                value:
-                    "{sendNotify:function(){},serverNotify:function(){},BarkNotify:function(){},tgBotNotify:function(){}}",
-            });
-        }
-}
-
+async function init_notify(Secrets, content, replacements) 
 
 module.exports = {
     replaceWithSecrets,
