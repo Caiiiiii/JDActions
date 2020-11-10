20fd80135982b1018173d181649990a8a3f239dd
jiegto <jiegto@outlook.com>
2020-11-10 15:05:19 +0800

    Update jd_fruit.js

--- function/jd_fruit.js
+++ function/jd_fruit.js
@@ -11,12 +11,6 @@ async function downFile () {
     await download(url, './')
 }
 
-async function changeFiele () {
-    let content = await fs.readFileSync('./jd_fruit.js', 'utf8')
-    content = content.replace(/'0a74407df5df4fa99672a037eec61f7e@dbb21614667246fabcfd9685b6f448f3@6fbd26cc27ac44d6a7fed34092453f77@61ff5c624949454aa88561f2cd721bf6'/, `''`)
-    content = content.replace(/'b1638a774d054a05a30a17d3b4d364b8@f92cb56c6a1349f5a35f0372aa041ea0@9c52670d52ad4e1a812f894563c746ea@8175509d82504e96828afc8b1bbb9cb3'/, `''`)
-    await fs.writeFileSync( './jd_fruit.js', content, 'utf8')
-}
 
 async function deleteFile(path) {
     // 查看文件result.txt是  否存在,如果存在,先删除
@@ -32,9 +26,6 @@ async function start() {
     // 下载最新代码
     await downFile();
     console.log('下载代码完毕')
-    // 替换变量
-    await changeFiele();
-    console.log('替换变量完毕')
     // 执行
     await exec("node jd_fruit.js >> result.txt");
     console.log('执行完毕')
